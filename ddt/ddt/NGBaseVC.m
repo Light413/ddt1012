476b0f52059a278206f8b6d8364d71ab031d900b
Light <light@sina.com>
2015-10-14 09:26:34 +0800

    ignore .DS_Store

--- ddt/ddt/NGBaseVC.m
+++ ddt/ddt/NGBaseVC.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSInteger index = self.tabBarController.selectedIndex;
+    NSLog(@"%d",index);
 }
 
 - (void)didReceiveMemoryWarning {
