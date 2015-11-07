ff17b620609bccb1f2f348f88c609737169a7d27
light pro <light@pro.com>
2015-11-07 20:55:20 +0800

    homesb conflict

--- ddt/ddt/NGShuaiDantVC.m
+++ ddt/ddt/NGShuaiDantVC.m
@@ -8,7 +8,12 @@
 
 #import "NGShuaiDantVC.h"
 
+#define btnbasetag  330
+
 @interface NGShuaiDantVC ()<UITextFieldDelegate,UITextViewDelegate>
+{
+    BOOL _textviewHasStart;
+}
 
 @property (weak, nonatomic) IBOutlet UITextField *tf_name;
 @property (weak, nonatomic) IBOutlet UIButton *btn_age;
@@ -38,8 +43,26 @@
     self.textviewbg.layer.borderColor = [UIColor lightTextColor].CGColor;
     self.textviewbg.layer.cornerRadius = 5;
     self.textviewbg.layer.masksToBounds = YES;
+    [self textviewDetaultDisp:NO];
+
     
 }
+
+-(void)textviewDetaultDisp:(BOOL)has
+{
+    if (!has) {
+        self.textview.text = @"详细说明: 户口所在地、社保、公积金、保单、资产情况、负债情况、工资形式、工资金额、工作年限、流水金额、借款用途、还款来源等详细说明。不要出现电话号码、QQ等其他联系方式";
+        self.textview.textColor = [UIColor lightGrayColor];
+            _textviewHasStart = NO;
+    }
+    else
+    {
+        self.textview.text = @"";
+        self.textview.textColor = [UIColor blackColor];
+            _textviewHasStart = YES;
+    }
+}
+
 
 -(void)awakeFromNib
 {
@@ -62,6 +85,35 @@
     
     
 }
+
+#pragma mark -- UITextViewDelegate
+
+-(void)textViewDidBeginEditing:(UITextView *)textView
+{
+    if (!_textviewHasStart) {
+        [self textviewDetaultDisp:YES];
+    }
+}
+
+-(void)textViewDidChange:(UITextView *)textView{
+
+    if (textView.text == 0) {
+      [self textviewDetaultDisp:NO];
+    }
+    if (textView.text.length<=100) {
+        self.textviewNumLab.text = [NSString stringWithFormat:@"%ld/100",textView.text.length];
+    }else{
+        self.textviewNumLab.text = @"100/100";
+    }
+}
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+    if (range.location>=100) {
+        return NO;
+    }else{
+        return YES;
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
